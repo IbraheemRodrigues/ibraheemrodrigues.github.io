322c9c4f211ac48323faf01644de33712162a4ef
IbraheemR <28712361+IbraheemR@users.noreply.github.com>
2019-07-05 19:15:13 +0100

    Add gel borders and glows to content images

--- assets/js/gel.js
+++ assets/js/gel.js
@@ -3,3 +3,8 @@ document.querySelectorAll(".card").forEach(function(el) {
   el.style.setProperty("--gel-color", `hsla(${color}, 100%, 50%, 0.3)`);
   el.style.setProperty("--border-color", `hsl(${color}, 100%, 50%)`);
 });
+
+document.querySelectorAll("#content img").forEach(function(el) {
+  var color = Math.floor(Math.random() * 360);
+  el.style.setProperty("--border-color", `hsl(${color}, 100%, 50%)`);
+});
