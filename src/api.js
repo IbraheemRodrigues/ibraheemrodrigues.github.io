eb736143902dfd7d2de6080c41745f8b0fc51842
Ibraheem Rodrigues <contact@ibraheemrodrigues.com>
2020-05-16 16:19:18 +0100

    Fix bad API

--- src/api.js
+++ src/api.js
@@ -1,1 +1,5 @@
-export default "http://localhost:5500/_export"
+// HACK: Change API here for local dev.
+//  Need to figure out better devops structure for this site.
+//  Make sure this is NEVER changed in a commit
+
+export default "http://content.ibraheemrodrigues.com"
